67cc2fbc5f8498dc5d4e3264ead90c63c7334aa3
Evgeniy <yaremenko.x@gmail.com>
2023-03-24 23:28:52 +0200

    function createImgGallery

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,25 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery')
+const galleryMarkup = createImgGallery(galleryItems);
+
+
+galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup)
+
+
+function createImgGallery (gallery) {
+    return gallery.map(({preview, original, description}) => {
+        return `
+        <li class="gallery__item">
+            <a class="gallery__link" href="large-image.jpg">
+                <img
+                class="gallery__image"
+                src='${preview}'
+                data-source='${original}'
+                alt='${description}'
+                />
+            </a>
+        </li>`
+    }).join('');
+}
